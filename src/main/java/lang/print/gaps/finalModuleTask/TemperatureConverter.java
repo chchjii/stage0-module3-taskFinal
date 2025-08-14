aba0ef1afca27b456dbb02f078686d352dddd24b
Kirill <cryt.mem@gmail.com>
2025-08-14 12:51:40 +0300

    sol

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double temperatureFahrenheit = temperatureCelsius * 1.8 + 32;
     }
 }
